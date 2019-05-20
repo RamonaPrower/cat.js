adfd760579cd57f6a1329429a2d9a4a917b3042e
Ramona Prower <garfieldfan3993@gmail.com>
2019-05-20 23:09:45 +0100

    feed easter egg

--- commands/commands/feed.js
+++ commands/commands/feed.js
@@ -1,10 +1,29 @@
 // imports
 const { GuildCat } = require('../../utils/cat');
+const strings = require('../../strings/cat.json');
+const badfood = response => {
+	return strings.badfood.some(i => {
+		const newReg = new RegExp(i, 'gmi');
+		return newReg.test(response.content);
+	});
+};
 // exports
 module.exports = {
 	async execute(message) {
-        const guildCat = await GuildCat.create(message.guild.id);
-        message.channel.send(guildCat.feed());
+	const guildCat = await GuildCat.create(message.guild.id);
+	const fedCatStr = guildCat.feed();
+	message.channel.send(fedCatStr)
+	.then(() => {
+		if (fedCatStr === '<:meowsip:578260722652413976>') {
+			message.channel.awaitMessages(badfood, { maxMatches: 1, time: 15000, errors: ['time'] })
+			.then(() => {
+				message.channel.send(strings.badfoodstring);
+			})
+			.catch(() => {
+				console.log('nothing');
+			});
+		}
+	});
 	},
 };
 
