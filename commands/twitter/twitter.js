e328485b4fb0a73211c92533174f468e46fff7df
Ramona Prower <garfieldfan3993@gmail.com>
2019-06-08 07:50:00 +0100

    drunken refactor of twitter

--- commands/twitter/twitter.js
+++ commands/twitter/twitter.js
@@ -3,59 +3,68 @@ const Twitter = require('twitter');
 const config = require('../../config.json');
 // exports
 module.exports = {
-	async execute(message) {
+    async execute(message) {
         const tclient = new Twitter({
-                consumer_key: config.ckey,
-                consumer_secret: config.csecret,
-                access_token_key: config.atkey,
-                access_token_secret: config.atsecret,
+            consumer_key: config.ckey,
+            consumer_secret: config.csecret,
+            access_token_key: config.atkey,
+            access_token_secret: config.atsecret,
         });
-        const newReg = /(?<!\|\|)(?<!<)https?:\/\/(?:mobile\.)?twitter\.com\/(?:#!\/)?(?:\w+)\/status(?:es)?\/(\d+)/gmi
-        const match = newReg.exec(message.content);
-        if (!match[1]) {return console.log('i could\'nt find a match')}
-        tclient.get('statuses/show', { id: match[1], tweet_mode: 'extended' }, async function(error, tweets) {
-            if (!error) {
-                // if there's less than one image, don't post
-
-                if (tweets.extended_entities) {
-                    if (tweets.extended_entities.media.length >= 2) {
-                        imgWebhookOrMessage(tweets.extended_entities.media.slice(1), message);
-                    }
-                }
-                if (tweets.is_quote_status === true){
-                    console.log('i found a quote tweet');
-                    let link = `https://twitter.com/${tweets.quoted_status.user.screen_name}/status/${tweets.quoted_status.id_str}`
-                    tclient.get('statuses/show', {id: tweets.quoted_status.id_str, tweet_mode: 'extended'}, function (error, tweets2) {
-                        if (tweets2.extended_entities) {
-                            if (tweets2.extended_entities.media.length >= 2){
-                                link = link.concat(' ', '(extra images omitted)');
-                                quoteWebhookOrMessage(link, message);
-                                return;
-                            }
-                            else {
-                                quoteWebhookOrMessage(link, message);
-                                return;
-                            }
-                        }
-                        else {
-                            quoteWebhookOrMessage(link, message);
-                            return;
-                        }
-                    })
-
-                }
-                if (error) {
-                    console.error(error)
-                }
-            }
-            if (error) {
-                console.error(`errored tweet is ${match[0]}`);
-                console.log(error);
-            }
-        });
+        matchTweets(tclient, message);
+        
     },
 
 };
+
+async function matchTweets(tclient, message) {
+    const newReg = /(?<!\|\|)(?<!<)https?:\/\/(?:mobile\.)?twitter\.com\/(?:#!\/)?(?:\w+)\/status(?:es)?\/(\d+)/gmi
+    const match = newReg.exec(message.content);
+    if (!match[1]) { return console.log('i could\'nt find a match') }
+    tclient.get('statuses/show', { id: match[1], tweet_mode: 'extended' }, async function (error, tweets) {
+        if (!error) {
+            handleTweet(tclient, tweets, message);
+
+        }
+        if (error) {
+            console.error(`errored tweet is ${match[0]}`);
+            console.error(error)
+        }
+    }
+    );
+}
+
+async function handleTweet(tclient, tweets, message) {
+    if (tweets.extended_entities) {
+        if (tweets.extended_entities.media.length >= 2) {
+            imgWebhookOrMessage(tweets.extended_entities.media.slice(1), message);
+        }
+    }
+    if (tweets.is_quote_status === true) {
+        console.log('i found a quote tweet');
+        handleQuoteTweet(tclient, tweets, message);
+    }
+}
+
+async function handleQuoteTweet(tclient, tweets, message) {
+    let link = `https://twitter.com/${tweets.quoted_status.user.screen_name}/status/${tweets.quoted_status.id_str}`
+    tclient.get('statuses/show', { id: tweets.quoted_status.id_str, tweet_mode: 'extended' }, function (error, tweets2) {
+        if (tweets2.extended_entities) {
+            if (tweets2.extended_entities.media.length >= 2) {
+                link = link.concat(' ', '(extra images omitted)');
+                quoteWebhookOrMessage(link, message);
+                return;
+            }
+            else {
+                quoteWebhookOrMessage(link, message);
+                return;
+            }
+        }
+        else {
+            quoteWebhookOrMessage(link, message);
+            return;
+        }
+    })
+}
 
 async function imgWebhookOrMessage(images, message) {
     if (message.guild.me.hasPermission('MANAGE_WEBHOOKS')) {
@@ -63,13 +72,13 @@ async function imgWebhookOrMessage(images, message) {
         if (hook.size !== 0 && hook.find('name', config.webhookname)) {
             const sendHook = hook.find('name', config.webhookname);
             await imgSendViaHook(images, message, sendHook);
-    
+
         }
         else if (!hook.find('name', config.webhookname)) {
             const sendHook = await message.channel.createWebhook(config.webhookname, './images/twitter.png');
             await imgSendViaHook(images, message, sendHook);
         }
-        else{
+        else {
             console.log('something went wrong with the webhooks');
         }
     }
@@ -80,7 +89,7 @@ async function imgWebhookOrMessage(images, message) {
         }
         await message.channel.send(data);
     }
-}
+};
 
 async function imgSendViaHook(images, message, hook) {
     const data = [];
@@ -88,7 +97,7 @@ async function imgSendViaHook(images, message, hook) {
         data.push(imageUrl.media_url);
     }
     await hook.send(data);
-}
+};
 
 async function quoteWebhookOrMessage(link, message) {
     if (message.guild.me.hasPermission('MANAGE_WEBHOOKS')) {
@@ -96,20 +105,20 @@ async function quoteWebhookOrMessage(link, message) {
         if (hook.size !== 0 && hook.find('name', config.webhookname)) {
             const sendHook = hook.find('name', config.webhookname);
             await quoteSendViaHook(link, sendHook);
-    
+
         }
         else if (!hook.find('name', config.webhookname)) {
             const sendHook = await message.channel.createWebhook(config.webhookname, './images/twitter.png');
             await quoteSendViaHook(link, sendHook);
         }
-        else{
+        else {
             console.log('something went wrong with the webhooks');
         }
     }
     else {
         await message.channel.send(`Found Quoted Tweet: `);
     }
-}
+};
 
 
 async function quoteSendViaHook(link, hook) {
@@ -117,11 +126,11 @@ async function quoteSendViaHook(link, hook) {
 }
 
 module.exports.info = {
-	name: 'Twitter',
-	description: 'Twitter auto-expansion',
-	summon: 'twitter link',
+    name: 'Twitter',
+    description: 'Twitter auto-expansion',
+    summon: 'twitter link',
 };
 module.exports.settings = {
     regexp: /(?<!\|\|)(?<!<)https?:\/\/(?:mobile\.)?twitter\.com\/(?:#!\/)?(?:\w+)\/status(?:es)?\/(\d+)/gmi,
-	tag: 'twitter',
-};
+    tag: 'twitter',
+}
