996b7a0a6f91ad3dfe60707faced6c3954adf349
Ramona Prower <garfieldfan3993@gmail.com>
2019-11-19 01:34:25 +0000

    Removed Redundant twitter features

--- commands/twitter/twitter.js
+++ commands/twitter/twitter.js
@@ -34,13 +34,7 @@ async function matchTweets(tclient, message) {
 }
 
 async function handleTweet(tclient, tweets, message) {
-    if (tweets.extended_entities) {
-        if (tweets.extended_entities.media.length >= 2) {
-            imgWebhookOrMessage(tweets.extended_entities.media.slice(1), message);
-        }
-    }
     if (tweets.is_quote_status === true) {
-        console.log('i found a quote tweet');
         handleQuoteTweet(tclient, tweets, message);
     }
 }
@@ -49,55 +43,12 @@ async function handleQuoteTweet(tclient, tweets, message) {
     let link = `https://twitter.com/${tweets.quoted_status.user.screen_name}/status/${tweets.quoted_status.id_str}`
     tclient.get('statuses/show', { id: tweets.quoted_status.id_str, tweet_mode: 'extended' }, function (error, tweets2) {
         if (tweets2.extended_entities) {
-            if (tweets2.extended_entities.media.length >= 2) {
-                link = link.concat(' ', '(extra images omitted)');
-                quoteWebhookOrMessage(link, message);
-                return;
-            }
-            else {
                 quoteWebhookOrMessage(link, message);
                 return;
             }
         }
-        else {
-            quoteWebhookOrMessage(link, message);
-            return;
-        }
-    })
+    )
 }
-
-async function imgWebhookOrMessage(images, message) {
-    if (message.guild.me.hasPermission('MANAGE_WEBHOOKS')) {
-        const hook = await message.channel.fetchWebhooks();
-        if (hook.size !== 0 && hook.find('name', config.webhookname)) {
-            const sendHook = hook.find('name', config.webhookname);
-            await imgSendViaHook(images, message, sendHook);
-
-        }
-        else if (!hook.find('name', config.webhookname)) {
-            const sendHook = await message.channel.createWebhook(config.webhookname, './images/twitter.png');
-            await imgSendViaHook(images, message, sendHook);
-        }
-        else {
-            console.log('something went wrong with the webhooks');
-        }
-    }
-    else {
-        const data = [];
-        for (const imageUrl of images) {
-            data.push(imageUrl.media_url);
-        }
-        await message.channel.send(data);
-    }
-};
-
-async function imgSendViaHook(images, message, hook) {
-    const data = [];
-    for (const imageUrl of images) {
-        data.push(imageUrl.media_url);
-    }
-    await hook.send(data);
-};
 
 async function quoteWebhookOrMessage(link, message) {
     if (message.guild.me.hasPermission('MANAGE_WEBHOOKS')) {
